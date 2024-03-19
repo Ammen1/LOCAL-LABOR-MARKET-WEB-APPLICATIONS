aeff4f4f20818fa9a4cafe34b52cc2a179b4f794
Tamirat <amenguda@gmail.com>
2024-03-20 00:36:51 +0300

    Create AppWrapper component and initialize React context
    Added an AppWrapper component to wrap the main App component and provide a context for managing user authorization state. Initialized a React context with default values for isAuthorized and user. Updated ReactDOM.render to use createRoot for rendering the application in a strict mode.

---
+++ frontend/src/main.jsx
@@ -0,0 +1,32 @@
+import React, { createContext, useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+export const Context = createContext({
+  isAuthorized: false,
+});
+
+const AppWrapper = () => {
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [user, setUser] = useState({});
+
+  return (
+    <Context.Provider
+      value={{
+        isAuthorized,
+        setIsAuthorized,
+        user,
+        setUser,
+      }}
+    >
+      <App />
+    </Context.Provider>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AppWrapper />
+  </React.StrictMode>
+);
