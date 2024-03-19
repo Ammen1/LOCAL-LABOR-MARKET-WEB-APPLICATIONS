46a1c9346c206d0b574594f229a449e455b10e1c
Tamirat <amenguda@gmail.com>
2024-03-20 00:32:42 +0300

    git commit -m "Add route to handle payment callback
    This commit adds a new route '/chapa-callback' to handle payment callbacks from the Chapa payment service. The route retrieves a single job document from the database using `findOne()`. If a job is found, it updates the `paid` status to true and saves the changes to the database. If no job is found, it

--- api/app.js
+++ api/app.js
@@ -8,6 +8,9 @@ import cors from "cors";
 import { errorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
+import { Job } from "./models/jobSchema.js";
+import transactionRoutes from "./routes/transactionRoutes.js"
+// const transactionRoutes = require('./routes/transactionRoutes');
 
 const app = express();
 config({ path: "./config/config.env" });
@@ -20,6 +23,8 @@ app.use(
   })
 );
 
+
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -30,10 +35,31 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
+
+app.post('/chapa-callback', async (req, res) => {
+  try {
+      const job = await Job.findOne(); // Use findOne() instead of find() to get a single document
+      if (job) {
+          job.paid = true;
+          await job.save(); // Save the changes to the database
+          console.log('Payment status updated for job:');
+          console.log(job);
+          res.status(200).json({ message: 'Payment status updated successfully' });
+      } else {
+          console.error('Job not found for transaction reference:');
+          res.status(404).json({ error: 'Job not found' });
+      }
+  } catch (error) {
+      console.error('Error updating payment status for job:', error);
+      res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
+app.use('/transactions', transactionRoutes);
 dbConnection();
-
 app.use(errorMiddleware);
+
 export default app;
