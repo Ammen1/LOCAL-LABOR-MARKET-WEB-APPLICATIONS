2af7a9c574f1f75a759ff2f7c16501138ac44086
Tamirat <amenguda@gmail.com>
2024-03-20 00:35:41 +0300

    feat: Implement routing and user authentication in App component
    Implemented routing using react-router-dom and user authentication logic in the App component. Used useEffect hook to fetch user data from the server upon component mount and updated the context based on user authorization status. Added routes for Login, Register, Home, Jobs, JobDetails, Application, MyApplications, Dashboard, PostJob, MyJobs, and NotFound components. Integrated react-hot-toast for displaying notifications. Also included Navbar and Footer components.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,67 @@
+import { useContext, useEffect } from "react";
+import "./App.css";
+import "tailwindcss/tailwind.css";
+import  "./index.css";
+import { Context } from "./main";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import { Toaster } from "react-hot-toast";
+import axios from "axios";
+import Navbar from "./components/Layout/Navbar";
+import Home from "./components/Home/Home";
+import Jobs from "./components/Job/Jobs";
+import JobDetails from "./components/Job/JobDetails";
+import Application from "./components/Application/Application";
+import MyApplications from "./components/Application/MyApplications";
+import PostJob from "./components/Job/PostJob";
+import NotFound from "./components/NotFound/NotFound";
+import MyJobs from "./components/Job/MyJobs";
+import Dashboard from "./pages/Dashboard";
+import Footer from "./components/Footer";
+
+const App = () => {
+  const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/user/getuser",
+          {
+            withCredentials: true,
+          }
+        );
+        setUser(response.data.user);
+        setIsAuthorized(true);
+      } catch (error) {
+        setIsAuthorized(false);
+      }
+    };
+    fetchUser();
+  }, [isAuthorized]);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/job/getall" element={<Jobs />} />
+          <Route path="/job/:id" element={<JobDetails />} />
+          <Route path="/application/:id" element={<Application />} />
+          <Route path="/applications/me" element={<MyApplications />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/job/post" element={<PostJob />} />
+          <Route path="/job/me" element={<MyJobs />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+        <Toaster />
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
