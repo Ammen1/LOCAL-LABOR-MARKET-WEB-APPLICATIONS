3f10c9e9f58bd07765043f85b5ec9b76694a8943
Tamirat <amenguda@gmail.com>
2024-03-19 12:17:18 +0300

    feat: Add user-related routes
    Added routes to retrieve users, job seekers, and employees from the database, as well as to delete a user by ID. Also included a route to get all users. The routes are as follows:
    - GET /api/getusers
    - GET /api/getjobseekers
    - GET /api/getbemployee
    - GET /api/getuserss
    - DELETE /api/delete/:id
    
    These routes provide various functionalities related to managing users within the system.

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, register, logout, getUser } from "../controllers/userController.js";
+import { login, register, logout, getUser, getUsers, getjobSeeker, getEmployees, getUserss, deleteUser} from "../controllers/userController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -8,5 +8,10 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", isAuthenticated, logout);
 router.get("/getuser", isAuthenticated, getUser);
+router.get("/getusers", getUsers);
+router.get("/getjobseekers", getjobSeeker);
+router.get("/getbemployee", getEmployees);
+router.get("/getuserss", getUserss)
+router.delete('/delete/:id', deleteUser)
 
 export default router;
