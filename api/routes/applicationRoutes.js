a153cfe08679cc180e5d03eb3c4c51560cb268f7
Tamirat <amenguda@gmail.com>
2024-03-19 12:16:19 +0300

    update getapplication router

--- api/routes/applicationRoutes.js
+++ api/routes/applicationRoutes.js
@@ -4,6 +4,7 @@ import {
   jobseekerDeleteApplication,
   jobseekerGetAllApplications,
   postApplication,
+  getAllApplications,
 } from "../controllers/applicationController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
@@ -13,5 +14,6 @@ router.post("/post", isAuthenticated, postApplication);
 router.get("/employer/getall", isAuthenticated, employerGetAllApplications);
 router.get("/jobseeker/getall", isAuthenticated, jobseekerGetAllApplications);
 router.delete("/delete/:id", isAuthenticated, jobseekerDeleteApplication);
+router.get("/applications", getAllApplications);
 
 export default router;
