2ff9f5ad4cb2e78b560fb0de3810f8d021e1c86a
Tamirat <amenguda@gmail.com>
2024-03-21 03:27:37 +0300

    feat: Implement Jobs component
    Implemented the Jobs component with functionality to fetch jobs from the backend, handle search queries for filtering jobs dynamically, and paginate the list of jobs for better navigation. Utilized debounce for optimizing search input performance and provided loading spinners and error handling for improved user experience. Styled the component using Tailwind CSS for a clean and responsive layout.

--- frontend/src/components/Job/Jobs.jsx
+++ frontend/src/components/Job/Jobs.jsx
@@ -1,13 +1,17 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../../main";
-import { Card } from "flowbite-react";
-
-
+import { Button, Spinner } from "flowbite-react";
+import debounce from 'lodash/debounce'; 
 
 const Jobs = () => {
   const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [jobsPerPage] = useState(8); 
   const { isAuthorized } = useContext(Context);
   const navigateTo = useNavigate();
 
@@ -16,8 +20,10 @@ const Jobs = () => {
       try {
         const response = await axios.get("http://localhost:4000/api/v1/job/getall", { withCredentials: true });
         setJobs(response.data.jobs);
+        setLoading(false);
       } catch (error) {
-        console.error(error);
+        setError(error.message);
+        setLoading(false);
       }
     };
     fetchJobs();
@@ -29,22 +35,73 @@ const Jobs = () => {
     }
   }, [isAuthorized, navigateTo]);
 
+
+  const debouncedSearch = debounce((searchQuery) => {
+    setSearchQuery(searchQuery);
+  }, 100); 
+
+  // Handle search input change
+  const handleSearchChange = (e) => {
+    const searchQuery = e.target.value;
+    debouncedSearch(searchQuery); 
+    setCurrentPage(1); 
+  };
+
+  // Filter jobs based on search query
+  const filteredJobs = jobs.filter((job) =>
+    job.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  // Pagination logic
+  const indexOfLastJob = currentPage * jobsPerPage;
+  const indexOfFirstJob = indexOfLastJob - jobsPerPage;
+  const currentJobs = filteredJobs.slice(indexOfFirstJob, indexOfLastJob);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <section className="jobs page">
-      <div className="mx-auto py-8">
-        <div size="2xl" className="text-center  mb-8">ALL AVAILABLE JOBS</div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {jobs.map((job) => (
-            <Card key={job._id} className="bg-white shadow-lg rounded-lg overflow-hidden">
-              <div className="p-6">
-                <p className="text-xl font-semibold mb-2">{job.title}</p>
-                <p className="text-lg mb-2">{job.category}</p>
-                <p className="text-lg mb-4">{job.country}</p>
-                <Link to={`/job/${job._id}`} className="text-blue-500 hover:underline">Job Details</Link>
-              </div>
-            </Card>
-          ))}
+    <section className="mt-20 flex justify-center items-center ">
+      <div className="mx-auto ">
+        <div size="2xl" className="text-center text-3xl mb-6 font-bold"> Book Your Next Task</div>
+        <div className="mb-4 border font-bold  w-full">
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={handleSearchChange}
+            placeholder="Describe One Task... for example, fix hole in my wall..."
+            className=" border-green-800 p-2 w-full font-bold "
+          />
         </div>
+        {loading ? (
+          <div className="flex justify-center items-center h-64 ">
+            <Spinner color="blue" />
+          </div>
+        ) : error ? (
+          <div className="text-red-600 text-center">{error}</div>
+        ) : (
+          <>
+            <div className="grid grid-cols-1 text-green-700 sm:grid-cols-2 lg:grid-cols-4 gap-1 justify-center items-center">
+              {currentJobs.map((job) => (
+                <Link to={`/job/${job._id}`} key={job._id}>
+                  <Button outline className="w-56 p-2 font-bold bg-white ">
+                    {job.title}  
+                  </Button>
+                </Link>
+              ))}
+            </div>
+            <div className="flex justify-center mt-4">
+              {Array.from({ length: Math.ceil(filteredJobs.length / jobsPerPage) }, (_, i) => (
+                <Button
+                  key={i}
+                  onClick={() => paginate(i + 1)}
+                  className={`mx-2 rounded-lg text-black ${currentPage === i + 1 ? 'bg-gradient-to-r from-indigo-500 to-pink-500 via-indigo-500 text-black' : 'hover:bg-gradient-to-r from-indigo-500 to-pink-500 hover:text-black'}`}
+                >
+                  {i + 1}
+                </Button>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </section>
   );
--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -1,9 +1,10 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../../main";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { Button, Navbar as BrowiserNav } from "flowbite-react";
 
 const Navbar = () => {
   const [show, setShow] = useState(false);
@@ -27,65 +28,69 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={`bg-[#1d4ed8] ${isAuthorized ? "block" : "hidden"}`}>
-
-      <div className="container mx-auto flex justify-between items-center ">
-        <div className="logo flex items-center">
-          <img src="/JobZee-logos__white.png" alt="logo" className="h-10 mr-4" />
+    <BrowiserNav className={`bg-blue-700 space-x-5 ${isAuthorized ? "block" : "hidden"}`}>
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex items-center ">
+        <Link
+        to="/"
+        className="self-center py-2 whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-2 bg-gradient-to-r from-indigo-500  rounded-lg text-white">
+        Local Labor  
+        </span>
+        Market
+      </Link>
         </div>
-        <ul className={!show ? "menu" : "show-menu menu "} >
+        <ul className={!show ? "menu" : "show-menu menu"}>
           <li className="hover:text-gray-300">
-            <Link to={"/"} onClick={() => setShow(false)} className="items-center space-x-4 text-white">
+            <Link to={"/"} onClick={() => setShow(false)} className="items-center space-x-4 text-white text-sm">
               Home
             </Link>
           </li>
           <li>
-            <Link to={"/job/getall"} onClick={() => setShow(false)}  className="items-center space-x-4 text-white">
+            <Link to={"/job/getall"} onClick={() => setShow(false)} className="items-center space-x-4 text-white text-sm">
               AllTasks
             </Link>
           </li>
           <li className=" text-sm">
-            <Link to={"/applications/me"} onClick={() => setShow(false)}  className="items-center space-x-4 text-white">
-              {user && user.role === "Employer"
-                ? "Applicant's Applications"
-                : "My Appliaction"}
+            <Link to={"/applications/me"} onClick={() => setShow(false)} className="items-center space-x-4 text-white text-sm">
+              {user && user.role === "Employer" ? "Applicant's Applications" : "My Appliaction"}
             </Link>
           </li>
-          {user && user.role === "Employer" ? (
+          {user && user.role === "Employer" && (
             <>
               <li>
-                <Link to={"/job/post"} onClick={() => setShow(false)}  className="items-center space-x-4 text-white">
+                <Link to={"/job/post"} onClick={() => setShow(false)} className="items-center space-x-4 text-white text-sm">
                   PostTask
                 </Link>
               </li>
               <li>
-                <Link to={"/job/me"} onClick={() => setShow(false)}  className="items-center space-x-4 text-white">
+                <Link to={"/job/me"} onClick={() => setShow(false)} className="items-center space-x-4 text-white text-sm">
                   ViewTasks
                 </Link>
               </li>
             </>
-          ) : (
-            <></>
           )}
-           {user && user.role === "Admin" ? (
+          {user && user.role === "Admin" && (
             <>
               <li>
-                <Link to={"/dashboard?tab=dash"} onClick={() => setShow(false)}  className="items-center space-x-4 text-white">
-                 Dashboard
+                <Link to={"/dashboard?tab=dash"} onClick={() => setShow(false)} className="items-center space-x-4 text-white text-sm">
+                  Dashboard
                 </Link>
               </li>
             </>
-          ) : (
-            <></>
           )}
-
-          <button  onClick={handleLogout}>logout</button>
+          <li>
+            <Button gradientDuoTone="purpleToBlue" className=" p-1"  onClick={handleLogout}>
+              Sign out
+            </Button>
+          </li>
         </ul>
         <div className="hamburger">
           <GiHamburgerMenu onClick={() => setShow(!show)} />
         </div>
       </div>
-    </nav>
+    </BrowiserNav>
   );
 };
 
