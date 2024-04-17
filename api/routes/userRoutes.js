9a6f36a6e9cdf2604575d7b59849da93c0f5be42
Tamirat <amenguda@gmail.com>
2024-04-17 04:58:27 +0300

    feat: Add routes for review functionality and update user routes
    Added routes for creating, retrieving, and deleting reviews in the user controller. Updated the user routes to include these new review routes. Now users can interact with reviews by creating, retrieving, and deleting them via the specified endpoints.
    
    These changes enhance the functionality of the application by providing endpoints to manage reviews and integrating them with the existing user routes.

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, register, logout, getUser, getUsers, getjobSeeker, getEmployees, getUserss, deleteUser} from "../controllers/userController.js";
+import { login, register, logout, getUser, createReview,getReviewById,   getUsers, getjobSeeker, getEmployees, getUserss, deleteUser,getAllReviews } from "../controllers/userController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -12,6 +12,12 @@ router.get("/getusers", getUsers);
 router.get("/getjobseekers", getjobSeeker);
 router.get("/getbemployee", getEmployees);
 router.get("/getuserss", getUserss)
-router.delete('/delete/:id', deleteUser)
+router.delete('/delete/:id', deleteUser);
+// router.post('/reviews', createReview);
+// router.post("/reviews",  createReview);
+router.post("/reviews",  createReview); // Create a review
+router.get("/reviews", getAllReviews); // Get reviews for a job seeker
+// router.get("/reviews/employer", getReviewsByEmployer); // Get reviews by an employer
+router.get("/reviews/:id",  getReviewById); // Delete a review
 
 export default router;
