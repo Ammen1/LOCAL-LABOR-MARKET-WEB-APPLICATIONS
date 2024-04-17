ef225cab3dc42063d1e459f662e722a1fb28a4e3
Tamirat <amenguda@gmail.com>
2024-04-17 04:57:06 +0300

    refactor: Refactor transaction schema for consistency and validation
    Modified the transaction schema to ensure consistency in field naming and added validation for required fields (email, first_name, last_name, currency, amount, and tx_ref) using Mongoose's schema options.
    
    Set a default value for the status field to 'pending' to indicate the initial status of transactions.
    
    These changes enhance the robustness and clarity of the transaction schema, ensuring that required fields are properly validated and providing a default status for transactions.

--- api/models/paymentModel.js
+++ api/models/paymentModel.js
@@ -1,17 +1,41 @@
 import mongoose from "mongoose";
 
 const transactionSchema = new mongoose.Schema({
-    email: String,
-    firstName: String,
-    lastName: String,
-    amount: Number,
-    txRef: String,
-    returnUrl: String,
-    customization: {
-      title: String,
-      description: String
-    },
-    status: String // This field can be used to track the status of the transaction (e.g., 'pending', 'success', 'failed')
-  });
-  
-  module.exports = mongoose.model('Transaction', transactionSchema);
+  email: {
+    type: String,
+    required: true
+  },
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  currency: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  tx_ref: {
+    type: String,
+    required: true
+  },
+  returnUrl: String,
+  customization: {
+    title: String,
+    description: String
+  },
+  status: {
+    type: String,
+    default: 'pending' // Default status can be set to 'pending'
+  }
+});
+
+export const Transaction = mongoose.model('Transaction', transactionSchema);
+
+
