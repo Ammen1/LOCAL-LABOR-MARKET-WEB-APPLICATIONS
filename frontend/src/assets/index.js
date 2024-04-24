2fb22d9ed2e1a53df5bf7785fc7813116fd70f4f
Tamirat <amenguda@gmail.com>
2024-04-25 00:00:31 +0300

    Add image imports
    - Imported various images for user profiles, social media icons, hero images, and backgrounds.

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,33 @@
+import NoProfile from "./userprofile.png";
+import HeroImage from "./hero.png";
+import Google from "./google.png";
+import Linkedin from "./linkedin.png";
+import Youtube from "./youtube.png";
+import WhatsApp from "./whatsapp.png";
+import Twitter from "./twitter.png";
+import Instagram from "./instagram.png";
+import Spotify from "./spotify.png";
+import Office from "./office.jpg";
+import Facebook from "./facebook.png";
+import CodeWave from "./codewave.png";
+import backgroundImage from "./backgroundImage.png";
+import backgroundImage1 from "./backgroundImage1.jpg";
+
+
+export {
+  backgroundImage1 ,
+  CodeWave,
+  backgroundImage,
+  NoProfile,
+  Facebook,
+  WhatsApp,
+  Twitter,
+  Instagram,
+  Spotify,
+  HeroImage,
+  Google,
+  Linkedin,
+  Youtube,
+  Office,
+
+};
