b151840880daeb07a16c12a7f5dec00c6169e013
Tamirat <amenguda@gmail.com>
2024-04-24 23:51:23 +0300

    Added routes for creating and fetching pins in Express using the Pin model. The POST route creates a new pin and the GET route retrieves all pins.

---
+++ api/routes/pinRoute.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+import { Pin } from "../models/Pin.js";
+const router = express.Router();
+
+//create a pin
+router.post("/", async (req, res) => {
+  const newPin = new Pin(req.body);
+  try {
+    const savedPin = await newPin.save();
+    res.status(200).json(savedPin);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get all pins
+router.get("/", async (req, res) => {
+  try {
+    const pins = await Pin.find();
+    res.status(200).json(pins);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+export default router;
