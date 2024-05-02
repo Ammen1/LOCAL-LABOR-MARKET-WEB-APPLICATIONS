a538475ffc3a5bc6379b4d017b6d7b8c74a92ea4
Tamirat <amenguda@gmail.com>
2024-05-02 09:21:08 +0300

    Add route to retrieve all transactions
    This commit adds a new route to the router configuration for handling GET requests to retrieve all transactions. The route is mapped to the `getalltransactio` controller function.
    
    The new route is configured to respond to GET requests to the root path ('/').
    
    This route provides a way to fetch all transactions from the server, facilitating the display of transaction data in the client application.

--- api/routes/transactionRoutes.js
+++ api/routes/transactionRoutes.js
@@ -1,11 +1,12 @@
 // routes/transactionRoutes.js
 
 import express from 'express';
-import { initiateTransaction, verifyTransaction } from '../controllers/transactionController.js';
+import { initiateTransaction, verifyTransaction, getalltransactio } from '../controllers/transactionController.js';
 
 const router = express.Router();
 
 router.post('/initiate', initiateTransaction);
 router.get('/verify/:txId', verifyTransaction);
+router.get('/', getalltransactio)
 
 export default router;
